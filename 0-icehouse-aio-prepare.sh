dc00721442976287dd563a48787e920838f6e020
To Thanh Cong <tcvn1985@gmail.com>
2014-06-22 12:25:06 +0700

    Khai bao cac dong echo

--- 0-icehouse-aio-prepare.sh
+++ 0-icehouse-aio-prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+echo "##### Thu hien update he thong truoc khi cai dat #####"
+sleep 3
+
 apt-get install -y python-software-properties &&  add-apt-repository cloud-archive:icehouse 
 
 apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
@@ -12,6 +15,9 @@ test -f $iphost.orig || cp $iphost $iphost.orig
 rm $iphost
 touch $iphost
 
+echo "##### Khai bao Hostname cho ubuntu #####"
+sleep 3
+
 hostname controller
 echo "controller" > /etc/hostname
  
@@ -22,11 +28,11 @@ $eth0_address	controller
 $eth1_address	controller
  
 # The following lines are desirable for IPv6 capable hosts
-::1     ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
+# ::1     ip6-localhost ip6-loopback
+# fe00::0 ip6-localnet
+# ff00::0 ip6-mcastprefix
+# ff02::1 ip6-allnodes
+# ff02::2 ip6-allrouters
 EOF
 
 
