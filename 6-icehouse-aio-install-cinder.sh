335abb75053a4402f13dc412291a4b39829153e3
Tô Thành Công <tcvn1985@gmail.com>
2014-06-25 09:17:52 +0700

    Script cai cinder

---
+++ 6-icehouse-aio-install-cinder.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -ex
+#
+RABBIT_PASS=Welcome123
+ADMIN_PASS=Welcome123
+TOKEN_PASS=Welcome123
+METADATA_SECRET=$TOKEN_PASS
+MYSQL_PASS=Welcome123
+#
+eth0_address=`/sbin/ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -f2 -d ":" `
+eth1_address=`/sbin/ifconfig eth1 | awk '/inet addr/ {print $2}' | cut -f2 -d ":" `
+MASTER=$eth0_address
+LOCAL_IP=$eth1_address
+GATEWAY_IP=192.168.1.1
+
+echo "##### Tao Physical Volume va Volume Group (tren disk sdb ) #####"
+sleep 3
+fdisk -l
+pvcreate /dev/sdb
+vgcreate cinder-volumes /dev/sdb
+
+#
+echo "##### Cai dat cac goi cho CINDER #####"
+sleep 3
+apt-get install -y cinder-api cinder-scheduler cinder-volume iscsitarget open-iscsi iscsitarget-dkms python-cinderclient
+
+
+echo "##### Cau hinh file cho cinder.conf #####"
+
+filecinder=/etc/cinder/cinder.conf
+test -f $filecinder.orig || cp $filecinder $filecinder.orig
+rm $filecinder
+touch $filecinder
+cat << EOF >> $filecinder
+[DEFAULT]
+rootwrap_config = /etc/cinder/rootwrap.conf
+api_paste_confg = /etc/cinder/api-paste.ini
+iscsi_helper = tgtadm
+volume_name_template = volume-%s
+volume_group = cinder-volumes
+verbose = True
+auth_strategy = keystone
+state_path = /var/lib/cinder
+lock_path = /var/lock/cinder
+volumes_dir = /var/lib/cinder/volumes
+rpc_backend = cinder.openstack.common.rpc.impl_kombu
+rabbit_host = $MASTER
+rabbit_port = 5672
+rabbit_userid = guest
+rabbit_password = $RABBIT_PASSWORD
+glance_host = $MASTER
+ 
+[database]
+connection = mysql://cinder:$MYSQL_PASS@$MASTER/cinder
+ 
+[keystone_authtoken]
+auth_uri = http://$MASTER:5000
+auth_host = $MASTER
+auth_port = 35357
+auth_protocol = http
+admin_tenant_name = service
+admin_user = cinder
+admin_password = $SERVICE_PASSWORD
+EOF
+
+# Phan quyen cho file cinder
+chown cinder:cinder $filecinder
+
+echo "##### Dong bo cho cinder #####"
+sleep 3
+cinder-manage db sync
+
+echo "##### Khoi dong lai CINDER #####"
+sleep 3
+service cinder-api restart
+service cinder-scheduler restart
+service cinder-volume restart
+
+echo "##### Hoan thanh viec cai dat CINDER ######
+
+
+
+
+
+
+
+
